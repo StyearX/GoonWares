8cc264f50c47a807b9ee34a91495f4e5d2908b68
StyearX <nazzz9442@gmail.com>
2026-07-11 13:10:38 +0800

    Create Skybox.lua

---
+++ Module/Skybox.lua
@@ -0,0 +1,320 @@
+BuiltInSkyboxes = {
+    ["Waguri"] = {
+        Folder = "Waguri",
+        Faces = {
+            { Prop = "SkyboxBk", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_ft.png", File = "waguri_ft.png" },
+            { Prop = "SkyboxFt", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_bk.png", File = "waguri_bk.png" },
+            { Prop = "SkyboxLf", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_rt.png", File = "waguri_rt.png" },
+            { Prop = "SkyboxRt", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_lf.png", File = "waguri_lf.png" },
+            { Prop = "SkyboxUp", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_up.png", File = "waguri_up.png" },
+            { Prop = "SkyboxDn", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Waguri/waguri_dn.png", File = "waguri_dn.png" },
+        },
+    },
+    ["Evernight"] = {
+        Folder = "Evernight",
+        Faces = {
+            { Prop = "SkyboxBk", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_RT.png", File = "SkyRt.png" },
+            { Prop = "SkyboxRt", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_LF.png", File = "SkyIf.png" },
+            { Prop = "SkyboxLf", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_BK.png", File = "SkyBk.png" },
+            { Prop = "SkyboxFt", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_FT.png", File = "SkyFt.png" },
+            { Prop = "SkyboxUp", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_UP.png", File = "SkyUp.png" },
+            { Prop = "SkyboxDn", Url = "https://raw.githubusercontent.com/StyearX/Custom-skybox/main/Evernight/evernight_DN.png", File = "SkyDn.png" },
+        },
+    },
+    ["Xenovia Quarta"] = {
+        Folder = "GoonWares Skyboxes/Xenovia Quarta",
+        Faces = {
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NjM0ODhf/if.png", File = "if.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NjM0ODlf/ft.png", File = "ft.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NjM0OTBf/dn.png", File = "dn.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NjM0OTFf/bk.png", File = "bk.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NjM0ODZf/up.png", File = "up.png" },
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NjM0ODdf/rt.png", File = "rt.png" },
+        },
+    },
+    ["Nino Nakano"] = {
+        Folder = "GoonWares Skyboxes/Nino Nakano",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NjQyODNf/right1.png", File = "right1.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NjQyNzhf/back.png", File = "back.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NjQyODBf/front.png", File = "front.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NjQyODJf/left1.png", File = "left1.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NjQyNzlf/down.png", File = "down.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NjQyODFf/up.png", File = "up.png" },
+        },
+    },
+    ["Nino Nakano 2"] = {
+        Folder = "GoonWares Skyboxes/Nino Nakano 2",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NjQyNTJf/rt.png", File = "rt.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NjQyNTBf/up.png", File = "up.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NjQyNTVf/dn.png", File = "dn.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NjQyNTNf/if.png", File = "if.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NjQyNTZf/bk.png", File = "bk.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NjQyNTRf/ft.png", File = "ft.png" },
+        },
+    },
+    ["Saki Saki"] = {
+        Folder = "GoonWares Skyboxes/Saki Saki",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NjQyNzNf/right.png", File = "right.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NjQyNzBf/left.png", File = "left.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NjQyNjhf/front1.png", File = "front1.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NjQyNjZf/back1.png", File = "back1.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NjQyNjdf/down1.png", File = "down1.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NjQyNjlf/Up1.png", File = "Up1.png" },
+        },
+    },
+    ["Rias Gremory"] = {
+        Folder = "GoonWares Skyboxes/Rias Gremory",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NzkyOTFf/RightRias.png", File = "RightRias.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NzkyOTNf/leftRias.png", File = "leftRias.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NzkyOTVf/front%20Rias.png", File = "frontRias.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NzkyOTRf/BackRias.png", File = "BackRias.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NzkyOThf/downRias.png", File = "downRias.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NzkyOTJf/UpRias.png", File = "UpRias.png" },
+        },
+    },
+    ["Yotsuba Nakano"] = {
+        Folder = "GoonWares Skyboxes/Yotsuba Nakano",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NzkzMzdf/YotsubaRt.png", File = "YotsubaRt.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NzkzMzhf/YotsubaUp.png", File = "YotsubaUp.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NzkzMzNf/YotsubaDn.png", File = "YotsubaDn.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NzkzMzRf/YotsubaFt.png", File = "YotsubaFt.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NzkzMzFf/YotsubaBk.png", File = "YotsubaBk.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NzkzMzZf/YotsubaLeft.png", File = "YotsubaLeft.png" },
+        },
+    },
+    ["Hakari Hananozo"] = {
+        Folder = "GoonWares Skyboxes/hk",
+        Faces = {
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NjEzMTVf/SkyBk.tex", File = "SkyBk.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/s/NjNfOTg0NjEzMTdf/SkyFt.tex", File = "SkyFt.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/s/NjNfOTg0NjEzMThf/SkyIf.tex", File = "SkyIf.png" },
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NjEzMTlf/SkyRt.tex", File = "SkyRt.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NjEzMjBf/SkyUp.tex", File = "SkyUp.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NjEzMTZf/Skydn.tex", File = "Skydn.png" },
+        },
+    },
+    ["Alya"] = {
+        Folder = "GoonWares Skyboxes/Alya",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NzkzMTZf/Rt.png", File = "Rt.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NzkzMTVf/Up.png", File = "Up.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NzkzMTJf/dn.png", File = "dn.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NzkzMTNf/ft.png", File = "ft.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NzkzMTFf/bk.png", File = "bk.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NzkzMTRf/if.png", File = "if.png" },
+        },
+    },
+    ["Alya 2"] = {
+        Folder = "GoonWares Skyboxes/Alya 2",
+        ResetHaze = true,
+        Faces = {
+            { Prop = "SkyboxRt", Url = "https://od.lk/d/NjNfOTg0NzkzMjRf/AlyaRt.png", File = "AlyaRt.png" },
+            { Prop = "SkyboxUp", Url = "https://od.lk/d/NjNfOTg0NzkzMjVf/AlyaUp.png", File = "AlyaUp.png" },
+            { Prop = "SkyboxDn", Url = "https://od.lk/d/NjNfOTg0NzkzMjFf/alyaDn.png", File = "alyaDn.png" },
+            { Prop = "SkyboxFt", Url = "https://od.lk/d/NjNfOTg0NzkzMjJf/AlyaFt.png", File = "AlyaFt.png" },
+            { Prop = "SkyboxBk", Url = "https://od.lk/d/NjNfOTg0NzkzMjBf/AlyaBk.png", File = "AlyaBk.png" },
+            { Prop = "SkyboxLf", Url = "https://od.lk/d/NjNfOTg0NzkzMjNf/AlyaLf.png", File = "AlyaLf.png" },
+        },
+    },
+    ["Iteration 3.2.0"] = {
+        RbxAssetIds = {
+            SkyboxLf = "rbxassetid://111969418190645",
+            SkyboxFt = "rbxassetid://86300370158690",
+            SkyboxRt = "rbxassetid://90660761614046",
+            SkyboxBk = "rbxassetid://94155581950702",
+            SkyboxDn = "rbxassetid://74377450776557",
+            SkyboxUp = "rbxassetid://110123630908028",
+        },
+        StarCount = 3000,
+        SunAngularSize = 0,
+        MoonAngularSize = 0,
+    },
+    ["IterationT Remake"] = {
+        RbxAssetIds = {
+            SkyboxLf = "rbxassetid://131895067742893",
+            SkyboxFt = "rbxassetid://139394863985793",
+            SkyboxRt = "rbxassetid://93313124175539",
+            SkyboxBk = "rbxassetid://93271230842735",
+            SkyboxDn = "rbxassetid://72142305760362",
+            SkyboxUp = "rbxassetid://86656757951125",
+        },
+        StarCount = 3000,
+        SunAngularSize = 0,
+        MoonAngularSize = 0,
+    },
+}
+
+function ApplyBuiltInSkybox(data)
+    for _, v in pairs(Lighting:GetChildren()) do
+        if v:IsA("Sky") then v:Destroy() end
+    end
+
+    if data.ResetHaze then
+        for _, v in pairs(Lighting:GetChildren()) do
+            if v:IsA("Atmosphere") then v.Haze = 0 end
+        end
+    end
+
+    local SkyInstance = Instance.new("Sky")
+    SkyInstance.Name = "Sky"
+    SkyInstance.CelestialBodiesShown = false
+    SkyInstance.StarCount = data.StarCount or 0
+    if data.SunAngularSize then SkyInstance.SunAngularSize = data.SunAngularSize end
+    if data.MoonAngularSize then SkyInstance.MoonAngularSize = data.MoonAngularSize end
+
+    if data.RbxAssetIds then
+        for prop, id in pairs(data.RbxAssetIds) do
+            SkyInstance[prop] = id
+        end
+    elseif data.Faces then
+        pcall(function() makefolder("GoonWares Skyboxes") end)
+        pcall(function() makefolder(data.Folder) end)
+        for _, face in ipairs(data.Faces) do
+            local path = data.Folder .. "/" .. face.File
+            if not isfile(path) then
+                writefile(path, game:HttpGet(face.Url, true))
+            end
+            SkyInstance[face.Prop] = getcustomasset(path)
+        end
+    end
+
+    SkyInstance.Parent = Lighting
+end
+
+SecEmoteChanger:AddSpace({ Height = 20 })
+
+SecEmoteChanger:AddDivider()
+SecEmoteChanger:AddSpace({ Height = 20 })
+
+SecSkyboxChanger = Tabs.Visual:AddSection("Skybox Changer", "solar/sun-bold")
+SecSkyboxChanger:AddSpace({ Height = 20 })
+SecSkyboxChanger:AddDivider()
+
+
+BuiltInSkyboxNames = {}
+for skyboxName in pairs(BuiltInSkyboxes) do
+    table.insert(BuiltInSkyboxNames, skyboxName)
+end
+table.sort(BuiltInSkyboxNames)
+
+SelectedBuiltInSkybox = BuiltInSkyboxNames[1]
+SecSkyboxChanger:AddDropdown("BuiltInSkyboxDropdown", {
+    DropdownOutsideWindow = true,
+    Search = true,
+    Title = "Built-in Skybox",
+    Values = BuiltInSkyboxNames,
+    Default = SelectedBuiltInSkybox,
+    Callback = function(value)
+        SelectedBuiltInSkybox = value
+    end
+})
+
+SecSkyboxChanger:AddButton({
+    Title = "Apply Skybox",
+    Callback = function()
+        local data = BuiltInSkyboxes[SelectedBuiltInSkybox]
+        if data then
+            local ok = pcall(ApplyBuiltInSkybox, data)
+            if ok then
+                Fluent:Notify({ Title = "Skybox", Content = SelectedBuiltInSkybox .. " applied.", Duration = 3 })
+            else
+                Fluent:Notify({ Title = "Skybox", Content = "Failed to apply skybox.", Duration = 3 })
+            end
+        end
+    end
+})
+
+SecSkyboxChanger:AddParagraph({
+    Title = "Built-in Skybox Info",
+    Content = "Your executors must have a write file/read file system, and for some skyboxes you will have to wait about 20 seconds to fully load the skybox."
+})
+
+SecSkyboxChanger:AddSpace({ Height = 20 })
+
+SecSkyboxChanger:AddDivider()
+SecSkyboxChanger:AddSpace({ Height = 20 })
+
+SecCustomSkybox = Tabs.Visual:AddSection("Custom Skybox", "solar/gallery-bold")
+SecCustomSkybox:AddSpace({ Height = 20 })
+SecCustomSkybox:AddDivider()
+
+
+CustomSkyboxInputs = { Lf = "", Rt = "", Up = "", Dn = "", Ft = "", Bk = "" }
+
+function ResolveSkyboxInput(value, faceName)
+    value = tostring(value or ""):gsub("^%s*(.-)%s*$", "%1")
+    if value == "" then return nil end
+    if value:match("^rbxassetid://") then
+        return value
+    elseif value:match("^%d+$") then
+        return "rbxassetid://" .. value
+    elseif value:match("^https?://") then
+        local ok = pcall(function() makefolder("GoonWares Custom Skybox") end)
+        local path = "GoonWares Custom Skybox/" .. faceName .. ".png"
+        local downloadOk = pcall(function()
+            writefile(path, game:HttpGet(value, true))
+        end)
+        if downloadOk then
+            return getcustomasset(path)
+        end
+    end
+    return nil
+end
+
+function ApplyCustomSkybox()
+    local propMap = { Lf = "SkyboxLf", Rt = "SkyboxRt", Up = "SkyboxUp", Dn = "SkyboxDn", Ft = "SkyboxFt", Bk = "SkyboxBk" }
+
+    for _, v in pairs(Lighting:GetChildren()) do
+        if v:IsA("Sky") then v:Destroy() end
+    end
+
+    local SkyInstance = Instance.new("Sky")
+    SkyInstance.Name = "Sky"
+    SkyInstance.CelestialBodiesShown = false
+    SkyInstance.StarCount = 0
+
+    local appliedAny = false
+    for face, prop in pairs(propMap) do
+        local resolved = ResolveSkyboxInput(CustomSkyboxInputs[face], face)
+        if resolved then
+            SkyInstance[prop] = resolved
+            appliedAny = true
+        end
+    end
+
+    if appliedAny then
+        SkyInstance.Parent = Lighting
+        Fluent:Notify({ Title = "Skybox", Content = "Custom skybox applied.", Duration = 3 })
+    else
+        SkyInstance:Destroy()
+        Fluent:Notify({ Title = "Skybox", Content = "No valid inputs provided.", Duration = 3 })
+    end
+end
+
+SecCustomSkybox:AddInput("CustomSkyboxLf", { Title = "Left (Lf)", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Lf = v end })
+SecCustomSkybox:AddInput("CustomSkyboxRt", { Title = "Right (Rt)", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Rt = v end })
+SecCustomSkybox:AddInput("CustomSkyboxUp", { Title = "Up", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Up = v end })
+SecCustomSkybox:AddInput("CustomSkyboxDn", { Title = "Down (Dn)", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Dn = v end })
+SecCustomSkybox:AddInput("CustomSkyboxFt", { Title = "Front (Ft)", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Ft = v end })
+SecCustomSkybox:AddInput("CustomSkyboxBk", { Title = "Back (Bk)", Placeholder = "rbxassetid or URL", Callback = function(v) CustomSkyboxInputs.Bk = v end })
+
+SecCustomSkybox:AddButton({
+    Title = "Apply Custom Skybox",
+    Callback = ApplyCustomSkybox
+})
+
+SecCustomSkybox:AddParagraph({
+    Title = "Custom Skybox Info",
+    Content = "You can upload 6 cube skyboxes to Roblox create or create 6 cubemaps as direct download links then enter the url or rbxassetid into the 6 inputs must be in the direction of the cube"
+})
