9256d40f46a2147772356dbacfad069c145d8932
StyearX <nazzz9442@gmail.com>
2026-07-29 19:19:19 +0800

    Update FloatingButtonModule.lua

--- Module/FloatingButtonModule.lua
+++ Module/FloatingButtonModule.lua
@@ -90,7 +90,7 @@ local function BuildFrame(ButtonName, SavedW, SavedH, Fluent)
     ScreenGui.Name = ButtonName
     ScreenGui.Parent = ButtonFolder
     ScreenGui.ResetOnSpawn = false
-    ScreenGui.DisplayOrder = 9998
+    ScreenGui.DisplayOrder = 1
     ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     ScreenGui.IgnoreGuiInset = false
 
